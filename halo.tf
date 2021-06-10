ac857e5b61c3b1c992208f34c48ae4d8e89b1af1
LOU Xun <aquarhead@ela.build>
2021-06-10 09:32:36 +0000

    Add stuff

--- halo.tf
+++ halo.tf
@@ -17,3 +17,28 @@ terraform {
 }
 
 provider "digitalocean" {}
+
+resource "digitalocean_droplet" "halo" {
+  image      = "ubuntu-20-04-x64"
+  name       = "haloword"
+  region     = "sfo3"
+  size       = "s-1vcpu-1gb-amd"
+  monitoring = true
+  ipv6       = true
+}
+
+resource "digitalocean_floating_ip" "halo" {
+  droplet_id = digitalocean_droplet.halo.id
+  region     = digitalocean_droplet.halo.region
+}
+
+data "digitalocean_project" "halo" {
+}
+
+resource "digitalocean_project_resources" "halos" {
+  project = data.digitalocean_project.halo.id
+  resources = [
+    digitalocean_droplet.halo.urn,
+    digitalocean_floating_ip.halo.urn,
+  ]
+}
